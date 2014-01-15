007ee14616ddc92b9aa28d28311c62a2446cf129
Zvika Ekhous <zvika.ekhous@jivesoftware.com>
2014-01-16 01:31:09 +0200

    Fixed problem with primitive returnType data types, also a bug with resolving the name of an array of an inner enum

--- restdl2code/src/main/java/com/zvoykish/restdl/generator/java/JavaWriter.java
+++ restdl2code/src/main/java/com/zvoykish/restdl/generator/java/JavaWriter.java
@@ -1,8 +1,6 @@
 package com.zvoykish.restdl.generator.java;
 
-import com.zvoykish.restdl.objects.CollectionObject;
-import com.zvoykish.restdl.objects.MapObject;
-import com.zvoykish.restdl.objects.TypedObject;
+import com.zvoykish.restdl.objects.*;
 
 import java.util.Map;
 
@@ -14,20 +12,29 @@ import java.util.Map;
  */
 public class JavaWriter {
     public String resolveTypeName(TypedObject type, Map<Long, TypedObject> typeMap) {
+        String className = type.getClassName();
         if (type instanceof CollectionObject) {
             TypedObject elementType = ((CollectionObject) type).getElementType();
             TypedObject resolvedType = typeMap.get(elementType.getId());
-            return type.getClassName() + '<' + resolveTypeName(resolvedType, typeMap) + '>';
+            return className + '<' + resolveTypeName(resolvedType, typeMap) + '>';
         }
         else if (type instanceof MapObject) {
             TypedObject keyType = ((MapObject) type).getKeyType();
             TypedObject valueType = ((MapObject) type).getValueType();
             TypedObject resolvedKeyType = typeMap.get(keyType.getId());
             TypedObject resolvedValueType = typeMap.get(valueType.getId());
-            return type.getClassName() + '<' + resolveTypeName(resolvedKeyType, typeMap) + ", " +
+            return className + '<' + resolveTypeName(resolvedKeyType, typeMap) + ", " +
                     resolveTypeName(resolvedValueType, typeMap) + '>';
+        } else if (type instanceof ArrayObject) {
+            TypedObject componentType = ((ArrayObject) type).getComponent();
+            TypedObject resolvedComponentType = typeMap.get(componentType.getId());
+            return resolveTypeName(resolvedComponentType, typeMap) + "[]";
+        }
+        else if (!className.contains(".") && !"void".equals(className) && !(type instanceof GenericDeclarationObject)) {
+            // Handle primitive data types (long, boolean, etc.)
+            return Character.toUpperCase(className.charAt(0)) + className.substring(1);
         }
 
-        return type.getClassName().replace('$', '_');
+        return className.replace('$', '_');
     }
 }
